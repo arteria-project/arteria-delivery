d43234fa409786c5cf03fcd5635f94db345a9d55
Monika Brandt <monika.brandt@medsci.uu.se>
2023-10-04 13:03:17 +0200

    Functions for parsing yaml-config and resolving re.
    Two new functions are added.
    One function for opening and reading a yaml-config. This function will also do string substitutions in the config file based on input values and resolved regular expression. The second function is for resolving regular expressions from the config option "filter".

--- delivery/services/organise_service.py
+++ delivery/services/organise_service.py
@@ -2,16 +2,19 @@
 import logging
 import os
 import time
+import sys
+import yaml
+import json
+import re
 
-from delivery.exceptions import ProjectAlreadyOrganisedException
+#from delivery.exceptions import ProjectAlreadyOrganisedException
 
-from delivery.models.project import RunfolderProject
-from delivery.models.runfolder import Runfolder, RunfolderFile
-from delivery.models.sample import Sample, SampleFile
-from delivery.services.file_system_service import FileSystemService
+#from delivery.models.project import RunfolderProject
+#from delivery.models.runfolder import Runfolder, RunfolderFile
+#from delivery.models.sample import Sample, SampleFile
+#from delivery.services.file_system_service import FileSystemService
 
 log = logging.getLogger(__name__)
-
 
 class OrganiseService(object):
     """
@@ -21,7 +24,8 @@ class OrganiseService(object):
     This service handles that in a synchronous way.
     """
 
-    def __init__(self, runfolder_service, file_system_service=FileSystemService()):
+    #def __init__(self, runfolder_service, file_system_service=FileSystemService()):
+    def __init__(self, runfolder_service, file_system_service=None):
         """
         Instantiate a new OrganiseService
         :param runfolder_service: an instance of a RunfolderService
@@ -207,3 +211,107 @@ class OrganiseService(object):
             read_no=sample_file.read_no,
             is_index=sample_file.is_index,
             checksum=sample_file.checksum)
+
+    def parse_yaml_config(config_file_path, input_key, input_value):
+        """
+        Read a configuration file for organizing a runfolder or analyzed project prior to delivery. 
+        This function will do string substitutions based on the input, keys in the configuration file
+         and results from resolved regular expressions (based on patterns from the configuration file). 
+
+        :param config_file_path: Full path to a configuration file in yaml-format.
+        :param input_key: projectid or runfolder_name
+        :param input_value: A runfolder name or a project name (for analysed projects).
+        :return: A list with one element per link/copy operation to be performed during organisation.
+         Each element consist of a tuple with 3 values ("path/to/source/file", "path/to/destination/file", "options as dictionary").
+        """
+
+        with open(config_file_path) as f:
+            config = yaml.load(f, Loader=yaml.SafeLoader)
+
+        input_dict = {}
+        input_dict[input_key] = input_value
+        input_dict.update(config["variables"]) #The input variable must be the first occuring key.
+        config["variables"] = input_dict
+        variables_dict = config["variables"]
+
+        config_as_list = []
+
+        for key in variables_dict.keys():
+            variables_dict[key] = variables_dict[key].format_map(Default(variables_dict))
+
+        for element in config["files_to_organise"]:
+            element["source"] = element["source"].format_map(Default(variables_dict))
+            element["destination"] = element["destination"].format_map(Default(variables_dict))
+
+            if "filter" in element["options"].keys(): 
+                element["options"]["filter"] = element["options"]["filter"].format_map(Default(variables_dict))
+                source_list = OrganiseService.resolve_config_regexp(element["options"]["filter"])
+                for source_file in source_list:
+                    source = ''.join([element["source"].strip('*'),source_file["source_file"]])
+                    destination_file = element["destination"].format_map(Default(source_file))
+                    destination = ''.join([destination_file, source_file["source_file"].split("/")[-1]])
+                    
+                    config_as_list.append((source,destination,element["options"]))
+
+            else:
+                config_as_list.append((element["source"],element["destination"],element["options"]))
+
+        return(config_as_list)
+
+    def resolve_config_regexp(regexp_pattern):
+        """
+        This function will parse through a list of files/paths and select all files/paths matching
+        the given regexp pattern.
+
+        :param regexp_pattern: A regular expression as specified in the configuration option "filter".
+        :return: A list where each element is a dictionary containing keys specified by the input regexp pattern.
+        The number of elements in the list is decided by the number of matches the re.search() returns. To each
+        list element, i.e. to each dictionary, the actual match (the source file) is also added.  
+        """
+        list_of_mock_files = OrganiseService.get_mock_file_list()
+        source_file_list = []
+        for file in list_of_mock_files:
+            search_re = re.search(regexp_pattern, file)
+            if search_re == None:
+                pass
+            else:
+                source_file = search_re.group()
+                source_file_dict = search_re.groupdict()
+                source_file_dict["source_file"] = source_file 
+                source_file_list.append(source_file_dict)
+  
+        return(source_file_list)
+
+    def get_mock_file_list():
+        """
+        Mock function for getting a list with all files found in on "source" specified in the config.
+        Only for testing purpose. Remove this function when code is getting intergated.
+        """
+        file_list = ["/proj/ngi2016001/incoming/200624_A00834_0183_BHMTFYDRXX/Unaligned/AB-1234/Sample_AB-1234-14092/AB-1234-14092_S35_L001_R1_001.fastq.gz",
+                     "/proj/ngi2016001/incoming/200624_A00834_0183_BHMTFYDRXX/Unaligned/AB-1234/Sample_AB-1234-14092/AB-1234-14092_S35_L001_R2_001.fastq.gz",
+                     "/proj/ngi2016001/incoming/200624_A00834_0183_BHMTFYDRXX/Unaligned/AB-1234/Sample_AB-1234-14597/AB-1234-14597_S35_L001_R1_001.fastq.gz",
+                     "/proj/ngi2016001/incoming/200624_A00834_0183_BHMTFYDRXX/Unaligned/AB-1234/Sample_AB-1234-14597/AB-1234-14597_S35_L001_R2_001.fastq.gz",
+                     "/proj/ngi2016001/incoming/200624_A00834_0183_BHMTFYDRXX/Unaligned/CD-5678/Sample_CD-5678-1/CD-5678-1_S89_L001_R1_001.fastq.gz",
+                     "/proj/ngi2016001/incoming/200624_A00834_0183_BHMTFYDRXX/Unaligned/CD-5678/Sample_CD-5678-1/CD-5678-1_S89_L001_R1_001.fastq.gz",
+                     "/proj/ngi2016001/incoming/200624_A00834_0183_BHMTFYDRXX/seqreports/project/AB-1234/200624_A00834_0183_BHMTFYDRXX_AB-1234_multiqc_report.html",
+                     "/proj/ngi2016001/incoming/200624_A00834_0183_BHMTFYDRXX/seqreports/project/CD-5678/200624_A00834_0183_BHMTFYDRXX_CD-5678_multiqc_report.html"] 
+
+        return file_list
+
+
+class Default(dict):
+    def __missing__(self, key):
+        return f"{{{key}}}"
+
+
+
+if __name__ == "__main__":
+    config_file_path = sys.argv[1]
+    input_key = sys.argv[2]
+    input_value = sys.argv[3]
+
+    if not os.path.exists(config_file_path):
+        raise Exception("Config file does not exist.")
+
+    config_as_list = OrganiseService.parse_yaml_config(config_file_path=config_file_path , input_key=input_key, input_value=input_value)
+    
